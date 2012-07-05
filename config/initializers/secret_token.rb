9ef4d8492418feb4315c55f6efd506c139c372ac
Tim Jensen <timdavidjensen@gmail.com>
2012-07-05 17:12:16 +1200

    Startup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blaja::Application.config.secret_token = '591db70e0377d54cbd1af0ff5b08b3c6f130d21e22c8e048a2c47986c9401125b038123cf8ce31849ee24f2b25d0ed76c6c76b5a4ca934415f27558b4eabd0dc'
