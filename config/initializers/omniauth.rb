25c3bca43566588891b163131f93cbe503180ecc
Tim Jensen <timdavidjensen@gmail.com>
2012-07-07 20:56:33 +1200

    ?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-OmniAuth.config.logger = Rails.logger
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['254892331288540'], ENV['ce5d7eb006a7ff34d5dba4a3e0b4f533']
 end
