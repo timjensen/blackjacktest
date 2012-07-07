dd240620f3187b62da928c6415b94e4fa4ee9b8b
Tim Jensen <timdavidjensen@gmail.com>
2012-07-08 09:18:45 +1200

    still workn with omni-auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['254892331288540'], ENV['ce5d7eb006a7ff34d5dba4a3e0b4f533']
+  provider :facebook, ENV['305048129591548'], ENV['a4634faf4656e427efe70de9fb6fbb32']
 end
 
