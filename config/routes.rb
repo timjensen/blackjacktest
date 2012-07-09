fbd67f6cece258880336ace80b543dc3342e04e5
Tim Jensen <timdavidjensen@gmail.com>
2012-07-09 21:30:28 +1200

    helpme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Blaja::Application.routes.draw do
   resources :game 
-  match 'auth/facebook/callback', :to => 'session#create'
+  match 'auth/facebook/callback', :to => 'session/create'
   match 'auth/failure', to: redirect('/')
   root :to => 'game#index'
   #root :to => 'game#index'
