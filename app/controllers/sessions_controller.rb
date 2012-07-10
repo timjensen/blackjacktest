553de776fe53cc0bfef1f8abd9e54c4cba427e4e
Tim Jensen <timdavidjensen@gmail.com>
2012-07-10 14:15:04 +1200

    wtf

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env['omniauth.auth'])
+    user = User.from_omniauth('omniauth.auth')
     session[:user_id] = user.id
     redirect_to '/game#index'
   end
