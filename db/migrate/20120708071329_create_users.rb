fc93e59c72a2cabe0f97a6f4010ca5b26c070b2f
Tim Jensen <timdavidjensen@gmail.com>
2012-07-15 22:46:52 +1200

    changed migrate

--- db/migrate/20120708071329_create_users.rb
+++ db/migrate/20120708071329_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :provider
-      t.string :uid
+      t.interger :uid
       t.string :name
       t.string :first_name
       t.string :oauth_token
